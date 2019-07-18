b98008c42671b51aade75783e70802b865a246ac
iTBoyer <itboyer@qq.com>
2019-07-18 09:55:09 +0800

    gaa

---
+++ hsg.app.Notepad/hsg_app_Notepad.h
@@ -0,0 +1,19 @@
+//
+//  hsg_app_Notepad.h
+//  hsg.app.Notepad
+//
+//  Created by admin on 2019/7/18.
+//  Copyright © 2019 recomend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hsg_app_Notepad.
+FOUNDATION_EXPORT double hsg_app_NotepadVersionNumber;
+
+//! Project version string for hsg_app_Notepad.
+FOUNDATION_EXPORT const unsigned char hsg_app_NotepadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hsg_app_Notepad/PublicHeader.h>
+
+
